6e7ab4a0109003c9fd0452f9d44f77912f43ff5c
IndikaMadhushan <indikamad.i@gmail.com>
2025-11-29 02:02:54 +0530

    init git

---
+++ src/test/java/com/healthcare/personal_health_monitoring/PersonalHealthMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.personal_health_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalHealthMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
