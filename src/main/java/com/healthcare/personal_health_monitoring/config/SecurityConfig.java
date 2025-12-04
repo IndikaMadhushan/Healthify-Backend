187663b094aecbe6bb86da093dbc1a4fcccf0fec
IndikaMadhushan <indikamad.i@gmail.com>
2025-12-04 12:37:02 +0530

    created the securityconfig class for avoid log in miss match since spring disable its security by seeing password encoder

---
+++ src/main/java/com/healthcare/personal_health_monitoring/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.healthcare.personal_health_monitoring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(httpBasic -> httpBasic.disable()); // disable Basic Auth
+
+        return http.build();
+    }
+}
