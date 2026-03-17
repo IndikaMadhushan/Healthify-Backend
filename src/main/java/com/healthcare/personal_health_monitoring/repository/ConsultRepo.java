d38895bdea52556633bf46b07c37b3434a385698
IndikaMadhushan <indikamad.i@gmail.com>
2026-03-17 16:28:36 +0530

    fix the bug

--- src/main/java/com/healthcare/personal_health_monitoring/repository/ConsultRepo.java
+++ src/main/java/com/healthcare/personal_health_monitoring/repository/ConsultRepo.java
@@ -3,17 +3,23 @@ package com.healthcare.personal_health_monitoring.repository;
 import com.healthcare.personal_health_monitoring.entity.ClinicBook;
 import com.healthcare.personal_health_monitoring.entity.ClinicPage;
 import com.healthcare.personal_health_monitoring.entity.ConsultPage;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @EnableJpaRepositories
 public interface ConsultRepo extends JpaRepository<ConsultPage,Integer>{
+    @EntityGraph(attributePaths = {"doctor", "doctor.user"})
     List<ConsultPage> findAllByPatient_Id(long patientId);
     List<ConsultPage> findByPatient_id(Long id);
+
+    @EntityGraph(attributePaths = {"doctor", "doctor.user", "patient", "patient.user"})
+    Optional<ConsultPage> findOneByConsultId(int consultId);
 
 }
 
